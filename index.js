d26735bb5e7e6004d5f28c4bdaf444e16e309707
root <danielmaloneygti@gmail.com>
2017-04-06 15:00:56 +0100

    Merge Fix

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-#!/usr/bin/env node
-
-const program = require('commander');
-const fs = require('fs');
-const path = require('path');
-=======
 #! /usr/bin/env node
 
-'use strict';
-
 const program = require('commander');
 const fs = require('fs');
->>>>>>> d2f46dbbfc37764733db66c785a60b9661ad7816
 const exec = require('child_process').exec;
 const jsonpkg = require('./package.json');
 const directory = require('./lib/createProject');
-
-<<<<<<< HEAD
 
 program
     .version('-v, --version', jsonpkg.version)
@@ -30,9 +18,6 @@ program.parse(process.argv);
 
 //name of the directory
 let appName = './' + program.name;
-=======
-let appName ="./" + program.name;
->>>>>>> d2f46dbbfc37764733db66c785a60b9661ad7816
 
 let packageJson = {
     name: appName.substring(2),
@@ -45,20 +30,6 @@ let packageJson = {
     }
 }
 
-<<<<<<< HEAD
-directory.createDirectory(appName);
-=======
-program
-    .version('-v, --version', jsonpkg.version)
-    .description('An express generator')
-    .usage('-n, <project name>')
-    .option('-n, --name <name>', 'Project Name')
-    .option('-u, --username <username>', 'Authentication')
-    .option('p, --password <password>', 'The user\'s password')
-
-program.parse(process.argv);
-
->>>>>>> d2f46dbbfc37764733db66c785a60b9661ad7816
 directory.createDirectory(appName, '/routes');
   
 console.log('This is the filesearch script.');
